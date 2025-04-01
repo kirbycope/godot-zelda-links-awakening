990f11dccc94ef13930fb54486c5b959a34157f1
Timothy Cope <kirbycope@gmail.com>
2025-04-01 14:50:36 -0700

    link

--- scenes/trendy_game.gd
+++ scenes/trendy_game.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var player: CharacterBody3D = $Player
 
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.camera_mount.rotation.x = deg_to_rad(-45)
+	player.camera_mount.rotation.x = deg_to_rad(-30)
 	player.lock_camera = true
