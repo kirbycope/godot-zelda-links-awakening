89e767f0ad74d06065dc7cb9bb5df6d31ac1dfa1
Timothy Cope <kirbycope@gmail.com>
2025-04-09 21:51:15 -0700

    fanfare

--- scenes/godot_plush.gd
+++ scenes/godot_plush.gd
@@ -3,4 +3,11 @@ extends RigidBody3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
-		print("pick up plush")
+		body.is_animation_locked = true
+		body.animation_player.play("fanfare")
+		position = body.position + Vector3(0.0, 2.0, 0.0)
+		rotation = Vector3.ZERO
+		freeze = true
+		# Delay execution
+		await get_tree().create_timer(3.0).timeout
+		queue_free()
