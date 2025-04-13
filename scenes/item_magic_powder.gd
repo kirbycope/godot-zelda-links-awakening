14f528d589e7076bb3079147369946e67aa61b12
Timothy Cope <kirbycope@gmail.com>
2025-04-12 18:40:59 -0700

    fanfare

--- scenes/item_magic_powder.gd
+++ scenes/item_magic_powder.gd
@@ -1,10 +1,15 @@
 extends RigidBody3D
 
+@onready var effects = $"../../Effects"
+var fanfare = preload("res://assets/sounds/key_item_fanfare.ogg")
+
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		body.is_animation_locked = true
 		body.animation_player.play("fanfare")
+		effects.stream = fanfare
+		effects.play()
 		position = body.position + Vector3(0.0, 2.0, 0.0)
 		rotation = Vector3.ZERO
 		freeze = true
